f4635a4b2c0db3af8b276c18fdd6955d794b978b
facy <dimas20pro07@gmail.com>
2025-05-24 04:04:14 +1000

    Create var_args.h

---
+++ lab11/var_args.h
@@ -0,0 +1,14 @@
+#ifndef VAR_ARGS_H
+#define VAR_ARGS_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+
+// Прототипы функций
+int sum(int count, ...);
+int max(int count, ...);
+int min(int count, ...);
+double average(int count, ...);
+
+#endif // VAR_ARGS_H
