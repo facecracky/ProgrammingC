8198a63bc5274836b8786a6c25c76fe7fe37440c
facy <dimas20pro07@gmail.com>
2025-05-27 03:23:30 +1000

    Update main.c

--- lab11/main.c
+++ lab11/main.c
@@ -5,7 +5,7 @@ int main() {
     printf("Сумма: %d\n", sum(5, 1, 2, 3, 4, 5));
     printf("Максимм: %d\n", max(4, 10, 30, 20, 5));
     printf("Минимум: %d\n", min(3, -5, 0, 8));
-    printf("Среднее: %.2f\n", average(5, 1, 2, 3, 4, 5));
+    printf("Сpеднеe: %.2f\n", average(5, 1, 2, 3, 4, 5));
 
     return 0;
 }
